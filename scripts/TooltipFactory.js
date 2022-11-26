c08c4668e5a5387419bd3d7cb7ed0b1c1b93cb09
Caelan <operation404@gmail.com>
2022-11-26 13:17:09 -0800

    formatting

--- scripts/TooltipFactory.js
+++ scripts/TooltipFactory.js
@@ -124,8 +124,7 @@ class TooltipFactory {
   }
   
   // public hook whenever a token is refreshed
-  async refreshToken(token, extra) {	
-    console.log(token);
+  async refreshToken(token) {	
     if (!token?.actor || !this._shouldActorHaveTooltip(token)) { return; }
     // Check if this refresh was caused by alt being pressed, and if so should tooltips render
     const altHeldRenderTooltip = this._isAltPressed() && this._getAltSettings().showOnAlt &&	
